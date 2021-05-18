bef5e8dd5925ebd99d96c08c1c0caf2638fb56c0
Vehikl Team <go@vehikl.com>
2021-05-18 10:44:42 -0400

    Add a board to the game

--- tests/feature/ConnectFour.spec.ts
+++ tests/feature/ConnectFour.spec.ts
@@ -2,9 +2,9 @@ import {mount, Wrapper} from "@vue/test-utils";
 import App from "@/App.vue";
 
 describe('Connect Four', () => {
-    it('Starts with no pieces on the board', () => {
+    it('Has a board', () => {
         const wrapper: Wrapper<App> = mount(App);
 
-        expect(wrapper.vm).toBeDefined();
+        expect(wrapper.findComponent({ref: 'game-board'}).exists()).toBe(true);
     });
 });
